ecacd4d476ef6bc95fbbddf532ebdedbdac79c2f
akarsh3007 <gupta.akarsh@gmail.com>
2017-04-07 05:09:03 -0400

    Code Optimization and added check for textOriginal property exists value

--- youtubesentimentalanalysis/src/main/java/group13/bigdata/youtubedatastream/Search.java
+++ youtubesentimentalanalysis/src/main/java/group13/bigdata/youtubedatastream/Search.java
@@ -1,4 +1,5 @@
 package group13.bigdata.youtubedatastream;
+
 import group13.bigdata.youtubedatastream.*;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -19,19 +20,17 @@ public class Search {
 	 * Define a global variable that identifies the name of a file that contains
 	 * the developer's API key.
 	 */
-	private static final String PROPERTIES_FILENAME = "youtube.properties";
-	private static final String API_KEY = "AIzaSyARdxaSF7zr04mQ50woOSEwB9MLG4B3Kms";
-
-	private static final long NUMBER_OF_COMMENTS_PER_VIDEO = 100;
+	private static YoutubeDataProducer producer;
 
 	public static void main(String[] args) {
 		try {
 
+			// initailize producer class to initalize config
+			producer = new YoutubeDataProducer();
 			// Prompt the user to enter a query term.
 			String queryTerm = getInputQuery();
 
-			URL url = new URL(
-					"https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + queryTerm + "&key=" + API_KEY);
+			URL url = new URL(Constants.VIDEO_BASE_URL + queryTerm + "&key=" + Constants.API_KEY);
 			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
 			StringBuffer json = new StringBuffer();
 			String line;
@@ -42,23 +41,22 @@ public class Search {
 			reader.close();
 
 			JSONObject jo = null;
-			
+
 			List<Video> videoList = new ArrayList<Video>();
-			
+
 			try {
 				jo = new JSONObject(json.toString());
-				JSONArray items = jo.getJSONArray("items");
-				
-				for (int i = 0; i < items.length(); i++) 
-				{
+				JSONArray items = jo.getJSONArray(Constants.ITEMS);
+
+				for (int i = 0; i < items.length(); i++) {
 					JSONObject obj = items.getJSONObject(i);
-					String video_title = obj.getJSONObject("snippet").getString("title");
-					String video_desc = obj.getJSONObject("snippet").getString("description");
-					String video_id = items.getJSONObject(i).getJSONObject("id").getString("videoId");
+					String video_title = obj.getJSONObject(Constants.SNIPPET).getString(Constants.VIDEO_TITLE);
+					String video_desc = obj.getJSONObject(Constants.SNIPPET).getString(Constants.VIDEO_DESC);
+					String video_id = items.getJSONObject(i).getJSONObject(Constants.ID).getString(Constants.VIDEO_ID);
 					ArrayList<Comment> commentList = new ArrayList<Comment>();
-					
-					URL url2 = new URL(
-							"https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults="+NUMBER_OF_COMMENTS_PER_VIDEO+"&videoId="+video_id+"&key=" + API_KEY);
+
+					URL url2 = new URL(Constants.COMMENT_BASE_URL + Constants.NUMBER_OF_COMMENTS_PER_VIDEO + "&videoId="
+							+ video_id + "&key=" + Constants.API_KEY);
 					BufferedReader reader2 = new BufferedReader(new InputStreamReader(url2.openStream()));
 					StringBuilder json2 = new StringBuilder();
 					String line2 = null;
@@ -67,34 +65,44 @@ public class Search {
 						json2.append(line2);
 					}
 					reader2.close();
-					
+
 					JSONObject comments = null;
 					try {
 						comments = new JSONObject(json2.toString());
 						json2.setLength(0);
-						JSONArray items2 = comments.getJSONArray("items");
+						JSONArray items2 = comments.getJSONArray(Constants.ITEMS);
 						// System.out.println(items);
 
 						for (int j = 0; j < items2.length(); j++) {
-							String comment_text = items2.getJSONObject(j).getJSONObject("snippet").getJSONObject("topLevelComment").getJSONObject("snippet").getString("textOriginal");
-							Comment com = new Comment(comment_text);
-							commentList.add(com);
+							String comment_text = null;
+							boolean isOriginalText = items2.getJSONObject(j).getJSONObject(Constants.SNIPPET)
+									.getJSONObject(Constants.TOP_LEVEL_COMMENT).getJSONObject(Constants.SNIPPET).has(Constants.COMMEXT_TEXT);
+							if (isOriginalText) {
+								comment_text = items2.getJSONObject(j).getJSONObject(Constants.SNIPPET)
+										.getJSONObject(Constants.TOP_LEVEL_COMMENT).getJSONObject(Constants.SNIPPET)
+										.getString(Constants.COMMEXT_TEXT);
+								Comment com = new Comment(comment_text);
+								StringJoiner joiner = new StringJoiner("|");
+								joiner.add(video_id).add(video_title).add(video_desc).add(comment_text);
+								producer.produceYouTubeData(Constants.TOPIC_NAME, joiner.toString());
+								commentList.add(com);
+
+							}
 						}
 					} catch (JSONException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-					
+
 					Video video = new Video(video_id, video_title, video_desc, commentList);
 					videoList.add(video);
 				}
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}			
+			}
 		} catch (IOException e) {
-			System.err.println(
-					"There was an error reading " + PROPERTIES_FILENAME + ": " + e.getCause() + " : " + e.getMessage());
+			System.err.println("There was an error reading " + ": " + e.getCause() + " : " + e.getMessage());
 			System.exit(1);
 		}
 	}
