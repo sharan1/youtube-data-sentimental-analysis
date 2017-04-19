adab0118e43799e09623777463d5caf8a32b82ad
akarsh3007 <gupta.akarsh@gmail.com>
2017-04-19 16:26:16 -0400

    Collecting Sentimenatal analysis from spark and publishing it to redis.

--- youtubedataprocessingspark/youtubedataprocessingspark/src/main/scala/group13/bigdata/youtubedataprocessingspark/App.scala
+++ youtubedataprocessingspark/youtubedataprocessingspark/src/main/scala/group13/bigdata/youtubedataprocessingspark/App.scala
@@ -16,18 +16,19 @@ import org.apache.spark.sql._;
 import org.apache.spark.sql.functions._;
 import org.apache.spark.sql.DataFrame;
 import com.databricks.spark.corenlp.functions._;
+import redis.clients.jedis.Jedis
 
 /**
  * @author ${user.name}
  */
 object App {
- 
+
   val config = new SparkConf().setAppName("youtube-sentimental-analysis").setMaster("local[2]").set("spark.driver.host", "localhost");
   val sparkContext = new SparkContext(config);
   val sqlContext = new SQLContext(sparkContext);
-  
+
   import sqlContext.implicits._;
-  
+
   def main(args: Array[String]) {
 
     val kafkaParams = Map[String, Object](
@@ -38,7 +39,7 @@ object App {
       "auto.offset.reset" -> "latest");
 
     val topic = Array("test");
-    
+
     sparkContext.setLogLevel("WARN");
 
     val streamingSparkContext = new StreamingContext(sparkContext, Seconds(3));
@@ -46,19 +47,29 @@ object App {
       streamingSparkContext,
       PreferConsistent,
       Subscribe[String, String](topic, kafkaParams));
-    
+
     stream.foreachRDD(rdd => {
       println("rdd partition size" + rdd.partitions.size + "with" + rdd.count());
       val xyz = rdd.map(record => record.value().split('|'))
-        .map(x=>(x(0),x(1),x(2),x(3)))
+        .map(x => (x(0), x(1), x(2), x(3)))
         .toDF("videoId", "videoTitle", "videoDescription", "comment")
         .withColumn("commentSentimentScore", sentiment(col("comment")))
         .groupBy("videoId", "videoTitle", "videoDescription")
         .agg(avg($"commentSentimentScore").as("videoSentimentScore"));
-      
-        xyz.show();
+      if (xyz.count() > 0) {
+        xyz.foreach { row => 
+            val message = (row.getAs("videoId"),row.getAs("videoTitle"),row.getAs("videoDescription"),row.getAs("videoSentimentScore"))
+            val jedis = new Jedis("localhost", 6379);
+            val pipleline = jedis.pipelined();
+            val write = message.productIterator.mkString("|");
+            val publishMessage = pipleline.publish("YouTube", write);
+            pipleline.sync();
+        
+        }
+      }
+
     });
-    
+
     streamingSparkContext.start();
     streamingSparkContext.awaitTermination();
   }
